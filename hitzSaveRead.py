d4f016e90a09e1e261bc1e54bdcfc7774d1db98b
Nick Bonfatti <nick.bonfatti@mathworks.com>
2014-11-21 13:44:22 -0500

    Revert "try this john"
    This reverts commit 28ea33b332419a69defd3159d61ba5ff7cee8c4e.

--- hitzSaveRead.py
+++ hitzSaveRead.py
@@ -3,51 +3,49 @@
 #
 # extract data from a PS2 NHL Hitz 20-02 save file (unknown if it works for xbox or GC saves)
 #
-
+ 
 import binascii
 import pprint
 import os
-
-FILENAME = 'SLUS_201.40_0.bin'
+ 
+#FILENAME = 'SLUS_201.40_0.bin'
 #PATH = '/media/b432a63a-9163-4aa8-938f-d55faaf624f2/playstation2/VMC/'
 
-#FILENAME = 'hitzsave'
+FILENAME = 'hitzsave'
 PATH = 'C:\saves'
 
 #if you have a save file directly, the offset is 8. if you have a OPL memory card image, the offset is 46080 + the 8 of the file
-#OFFSET = 8
-OFFSET = 46088
-
+OFFSET = 8
+#OFFSET = 46088
+ 
 # Converts the hex string to an integer value
 
-def convert(hexString):
+def convert(hexString):  
 	return int(binascii.hexlify(hexString),16)
 
-def hitzSaveRead(filename):
-    global OFFSET
+def hitzSaveRead(filename): 
     with open(filename,'rb') as fp:
         contents=fp.read()
         fp.close()
-
+     
     #strip header
     contents=contents[OFFSET:]
-
+     
     players={}
-
-
+     
+     
     namelength=5
-    gamesplayedPosition=10
+    gamesplayedPosition=10 
     shotsPosition=12 # 2 bytes
     goalsPosition=14 # 2 bytes
     assistsPosition=16
     consecutivelossesPosition=20
     hitsPosition=32 # 2 bytes
     winsPosition=34
-
+     
     for i in range(20): # there's a max of 20 users stored in a save
-
+        
         startpos = i * 304
-        print str(len(contents)) + ' ' + str(startpos) +  ' '+str(gamesplayedPosition)
         name=contents[startpos:startpos+5].rstrip('\x00')
         gamesplayedhex=contents[startpos+gamesplayedPosition]
         shotshex=contents[startpos+shotsPosition+1:startpos+shotsPosition-1:-1]       # reversed to fix endianness
@@ -56,7 +54,7 @@ def hitzSaveRead(filename):
         winshex = contents[startpos + winsPosition]
         assistshex = contents[startpos+assistsPosition]
         consecutivelosseshex = contents[startpos+consecutivelossesPosition]
-
+     
         gamesplayed=convert(gamesplayedhex)
         shots=convert(shotshex)
         goals = convert(goalshex)
@@ -77,5 +75,5 @@ def hitzSaveRead(filename):
     return players
 
 if __name__ == '__main__':
-    players = hitzSaveRead(os.path.join(PATH,FILENAME))
+    players = hitzSaveRead(os.path.join(PATH,FILENAME)) 
     pprint.pprint(players)
