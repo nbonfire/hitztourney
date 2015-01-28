a2e8afc9afc9290b17254dacbed8aa5ed202d3df
johnz1 <zillioxj@gmail.com>
2015-01-28 12:09:36 -0500

    Increase time to wait before triggering hitzSaveRead to 30 secs

--- hitzSaveMonitor.py
+++ hitzSaveMonitor.py
@@ -26,7 +26,7 @@ DIRNAME = '/media/b432a63a-9163-4aa8-938f-d55faaf624f2/playstation2/VMC/'
 #FILENAME = 'hitzsave'
 #DIRNAME = 'C:\saves'
 
-SECONDS_TO_WAIT_FOR_FILE_COMPLETE=20
+SECONDS_TO_WAIT_FOR_FILE_COMPLETE=30
 
 players = hitzSaveRead(os.path.join(DIRNAME,FILENAME))
 #print players
